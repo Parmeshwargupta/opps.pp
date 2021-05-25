7a04a78d54ef35000bc482f8e07d143331dad764
Parmeshwar Gupta <60055326+Parmeshwargupta@users.noreply.github.com>
2021-05-25 20:36:32 +0530

    Program :8: shape class

---
+++ oops8.cpp
@@ -0,0 +1,50 @@
+// Parmeshwar (191920) b. tech (cse) 4th sem
+
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+    public:
+    virtual void numberOfSides() = 0 ;
+
+};
+class Trapezoid:public Shape
+{
+    public:
+      void numberOfSides()
+        {
+               cout<<"number of sides in trapezoid : 4 "<<endl;
+        }
+};
+class Triangle:public Shape
+{
+    public:
+
+     void numberOfSides()
+        {
+               cout<<"number of sides in triangle : 3 "<<endl;
+        }
+
+};
+class Hexagon:public Shape
+{
+    public:
+     void numberOfSides()
+        {
+               cout<<"number of sides in hexagon : 6 "<<endl;
+        }
+
+};
+
+int main()
+{
+    Trapezoid t;
+    Triangle tr;
+    Hexagon h;
+    t.numberOfSides();
+    tr.numberOfSides();
+    h.numberOfSides();
+
+     return 0;
+}
